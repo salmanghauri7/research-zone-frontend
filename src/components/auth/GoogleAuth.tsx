427fb3214d7bd9d6e99c19b27dad9243f53f5e4e
salmanghauri7 <salmanghauri62@gmail.com>
2025-12-21 12:02:35 +0500

    commented the googleAuth component because it was not being used

--- src/components/auth/GoogleAuth.tsx
+++ src/components/auth/GoogleAuth.tsx
@@ -1,30 +1,30 @@
-"use client";
-import { useGoogleLogin, TokenResponse } from "@react-oauth/google";
-import { useState } from "react";
-import { FcGoogle } from "react-icons/fc";
+// "use client";
+// import { useGoogleLogin, TokenResponse } from "@react-oauth/google";
+// import { useState } from "react";
+// import { FcGoogle } from "react-icons/fc";
 
-const GoogleAuth = ({ authType }: { authType: string }) => {
-  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean | null>(null);
+// const GoogleAuth = ({ authType }: { authType: string }) => {
+//   const [isGoogleLoading, setIsGoogleLoading] = useState<boolean | null>(null);
 
-  const googleAuth = useGoogleLogin({
-    flow: "auth-code",
-    onSuccess: (response: TokenResponse) => {
-      console.log(response);
-      setIsGoogleLoading(true);
-    },
-    onError: () => console.log("failed"),
-  });
+//   const googleAuth = useGoogleLogin({
+//     flow: "auth-code",
+//     onSuccess: (response: TokenResponse) => {
+//       console.log(response);
+//       setIsGoogleLoading(true);
+//     },
+//     onError: () => console.log("failed"),
+//   });
 
-  return (
-    <button
-      type="button"
-      onClick={() => googleAuth()}
-      className="w-full flex items-center justify-center border border-gray-300 rounded-lg py-2 hover:bg-gray-100 transition cursor-pointer"
-    >
-      <FcGoogle size={22} className="mr-2" />
-      {authType} with Google
-    </button>
-  );
-};
+//   return (
+//     <button
+//       type="button"
+//       onClick={() => googleAuth()}
+//       className="w-full flex items-center justify-center border border-gray-300 rounded-lg py-2 hover:bg-gray-100 transition cursor-pointer"
+//     >
+//       <FcGoogle size={22} className="mr-2" />
+//       {authType} with Google
+//     </button>
+//   );
+// };
 
-export default GoogleAuth;
+// export default GoogleAuth;
