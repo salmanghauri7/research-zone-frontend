782b0df4a7da43effbf9c60ed1a2861325827aec
salmanghauri7 <salmanghauri62@gmail.com>
2026-02-06 10:26:56 +0500

    fix(next.config): correct API destination path in rewrites configuration

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*", // When the browser requests /api/...
-        destination: "https://13.205.7.218.sslip.io/:path*", // ...Next.js forwards it here
+        destination: "https://13.205.7.218.sslip.io/api/:path*", // ...Next.js forwards it here
       },
     ];
   },
