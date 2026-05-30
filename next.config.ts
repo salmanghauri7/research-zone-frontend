3d36dde2ff5205dd95ae0f977cdd00f256666893
salman <salmanghauri62@gmail.com>
2026-05-30 14:11:56 +0500

    fix(next.config): update API base URL to the correct production endpoint

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   async rewrites() {
     const apiUrl =
       process.env.NEXT_PUBLIC_BASE_URL_API_PROD ||
-      "https://13.205.7.218.sslip.io";
+      "https://research-zone-backend.onrender.com";
     return [
       {
         source: "/api/:path*", // When the browser requests /api/...
